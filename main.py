efbd67b97e6699e2b187eb53d1839daae81b3d80
AlekseyGarev <garev91@gmail.com>
2025-06-26 16:03:14 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# import sys
-# sys.path.append("C:/project/Buxgalteria")
-
-from application.salary import calculate_salary
-from application.db.people import get_employees
-
-if __name__ == '__main__':
-    calculate_salary()
-    get_employees()
